051736633feb9b345424f32dd7756371462d488e
Muntazir <muntazir@muntazirs-macbook.local>
2015-10-27 12:35:05 +0530

    Initial Commit

---
+++ Hjejni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hjejni
+//
+//  Created by Muntazir on 27/10/15.
+//  Copyright © 2015 Hjejni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
